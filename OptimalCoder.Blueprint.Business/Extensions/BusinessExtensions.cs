ba0ac14cd71b4800fb36e8a25031a2b2c9549453
mirnes-mrkaljevic <mrkaljevicmirnes@gmail.com>
2026-08-15 20:46:49 +0200

    Domain -> Business

---
+++ OptimalCoder.Blueprint.Business/Extensions/BusinessExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace OptimalCoder.Blueprint.Business.Extensions
+{
+    public static class BusinessExtensions
+    {
+        public static IServiceCollection AddBusinessServices(this IServiceCollection services)
+        {
+
+
+            return services;
+        }
+    }
+}
